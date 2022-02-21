462a8ec5e5cfbe63567be9e474513ca699e786d3
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-21 22:28:59 +0100

    fix?

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,7 +9,7 @@ const Home = () => {
         <link rel="icon" href="/favicon.ico" />
         <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
         <script>
-          if (window.netlifyIdentity) {
+          if (window && window.netlifyIdentity) {
           window.netlifyIdentity.on("init", user => {
             if (!user) {
               window.netlifyIdentity.on("login", () => {
