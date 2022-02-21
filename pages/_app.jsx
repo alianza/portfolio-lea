a66e51708d747ef90e3f6b162ed936f4457a6f5e
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-22 00:28:27 +0100

    cleanup

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -5,18 +5,19 @@ import NextNProgress from "nextjs-progressbar"
 import Head from "next/head"
 import { useState } from "react"
 
-function MyApp({ Component, pageProps }) {
+function MyApp({Component, pageProps}) {
   const [darkTheme, setDarkTheme] = useState(false)
 
   useDarkThemeListener(setDarkTheme)
 
-  return <Layout>
-    <Head>
-      <meta name="theme-color" content={darkTheme ? '#ffe000' : '#232323'}/>
-    </Head>
-    <NextNProgress color={darkTheme ? '#232323' : '#ffe000'}/>
-    <Component {...pageProps} />
-  </Layout>
+  return (
+    <Layout>
+      <Head>
+        <meta name="theme-color" content={darkTheme ? '#ffe000' : '#232323'}/>
+      </Head>
+      <NextNProgress color={darkTheme ? '#232323' : '#ffe000'}/>
+      <Component {...pageProps} />
+    </Layout>)
 }
 
 export default MyApp
