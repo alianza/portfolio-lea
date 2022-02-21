c55c819e7ae312750a3effa02d7af0fca600adb8
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-22 00:28:22 +0100

    Fix build

---
+++ pages/blog/index.jsx
@@ -0,0 +1,15 @@
+const Blog = () => {
+  return (
+    <div>
+      <h1>Blog</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod
+        nisl euismod, porttitor nunc eu, tincidunt nisl. Nullam euismod
+        lacinia nunc, eu aliquam eros. Nullam euismod lacinia nunc, eu
+        aliquam eros.
+      </p>
+    </div>
+  );
+};
+
+export default Blog;
