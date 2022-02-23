7a24db01af5c4b71867c4695b5ad92b97914a783
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-23 18:38:23 +0100

    Added navLink component

---
+++ components/navLink/navLink.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import utils from '../../styles/utils.module.scss'
+
+const NavLink = ({ label, href, pathName }) => {
+  const router = useRouter()
+
+  !pathName && (pathName = href)
+
+  return (
+    <Link href={href}><a className={router.pathname === pathName ? utils.active : ""}>{label}</a></Link>
+  )
+}
+
+export default NavLink;
