705a719f5ebc6329d3ff1e94d2724bb06d35e0d4
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-25 06:36:06 +0100

    Refactored contact page styles

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,6 +1,7 @@
 import utils from "../../styles/utils.module.scss"
 import Layout from "../../components/layout/layout/layout"
 import layoutData from "../../content/config.json"
+import styles from "./contact.module.scss"
 
 export const getStaticProps = async () => {
   return {
@@ -18,52 +19,30 @@ const Contact = () => {
           name="contact"
           method="POST"
           data-netlify="true"
-          className="form">
+          className={styles.form}>
           <input type="hidden" name="form-name" value="contact"/>
           <p>
             <label>
-              <span>Your Name:</span> <input type="text" name="name" required />
+              <span>Your Name:</span>
+              <input type="text" name="name" required />
             </label>
           </p>
           <p>
             <label>
-              <span>Your Email:</span> <input type="email" name="email" required />
+              <span>Your Email:</span>
+              <input type="email" name="email" required />
             </label>
           </p>
           <p>
             <label className="items-start">
-              <span>Your Message to me:</span> <textarea name="email" />
+              <span>Your Message to me:</span>
+              <textarea name="email" />
             </label>
           </p>
           <p>
             <button type="submit">Send</button>
           </p>
         </form>
-      <style jsx >{`
-        .form {
-            @apply w-[36em];
-            
-            p {
-              @apply mb-4 w-full;
-              
-              label {
-                @apply block flex justify-between items-center;
-                
-                span {
-                @apply mr-2;
-                }
-              }
-              
-              input, textarea {
-                @apply border rounded w-[24em];
-              }
-              
-              textarea {
-                @apply h-24;
-              }
-            }
-        }
-      `}</style>
     </div>
   )
 }
