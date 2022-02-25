fd04b5d3491d8194834133cc02cb1d6d0e336cbf
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-25 06:35:34 +0100

    Fix blockquotes & mainTitle option

--- components/mdContent/MDContent.jsx
+++ components/mdContent/MDContent.jsx
@@ -1,18 +1,18 @@
 import React from "react"
 import contentStyles from "../../public/styles/content.module.css"
 
-const MDContent = ({ content: { title, date, thumbnail, category, content }, noDate } ) => {
+const MDContent = ({ content: { title, date, thumbnail, category, content }, noDate, mainTitle } ) => {
   return (
-    <div className={contentStyles.content}>
-      <h1>{title}</h1>
+    <article className={contentStyles.content}>
+      <h1 className={mainTitle ? contentStyles.mainTitle : ''}>{title}</h1>
       <div className="flex w-full justify-between items-center mb-2">
         {!noDate && <p className="m-0">{date}</p>}
         {category && <p className="m-0 rounded bg-accent-2 transition-colors py-1 px-2">{category}</p>}
       </div>
       {thumbnail && <img alt='thumbnail' src={thumbnail}/>}
-      <article dangerouslySetInnerHTML={{ __html: content }}/>
-    </div>
+      <div className="relative z-[1]" dangerouslySetInnerHTML={{ __html: content }}/>
+    </article>
   )
 }
 
-export default MDContent
+export default MDContent;
