47b646bc91bb79f62eddf04168953041fbe223cf
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-27 00:15:59 +0100

    Content preview styles in scss

--- components/mdContent/MDContent.jsx
+++ components/mdContent/MDContent.jsx
@@ -1,17 +1,20 @@
 import React from "react"
-import contentStyles from "../../public/styles/content.module.css"
+import contentStyles from "./MDContent.module.scss"
 
-const MDContent = ({ content: { title, date, thumbnail, category, content }, noDate, mainTitle } ) => {
+const MDContent = ({ content: { title, date, thumbnail, category, content }, noDate, withSpacing } ) => {
   return (
+    <>
     <article className={contentStyles.content}>
-      <h1 className={mainTitle ? contentStyles.mainTitle : ''}>{title}</h1>
-      <div className="flex w-full justify-between items-center mb-2">
-        {!noDate && <p className="m-0">{date}</p>}
-        {category && <p className="m-0 rounded bg-accent-2 transition-colors py-1 px-2">{category}</p>}
+      <h1 className={contentStyles.mainTitle}>{title}</h1>
+      <div className={contentStyles.metaData}>
+        {!noDate && <time className="m-0">{date}</time>}
+        {category && <span className={contentStyles.categoryLabel}>{category}</span>}
       </div>
-      {thumbnail && <img alt='thumbnail' src={thumbnail}/>}
+      {thumbnail && <img className="w-full" alt='thumbnail' src={thumbnail}/>}
       <div className="relative z-[1]" dangerouslySetInnerHTML={{ __html: content }}/>
     </article>
+      {withSpacing && <hr className="my-4"/>}
+    </>
   )
 }
 
