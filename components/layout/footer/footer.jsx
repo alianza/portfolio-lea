aa9413a84e170d1091db9fc1bca9dfa0b4270370
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-27 16:15:47 +0100

    Updated footer

--- components/layout/footer/footer.jsx
+++ components/layout/footer/footer.jsx
@@ -1,13 +1,12 @@
 import React from "react"
 import utils from '../../../styles/utils.module.scss'
-import Sun from "../../svg/sun"
-import Moon from "../../svg/moon"
+import DarkModeButton from "../../darkModeButton/darkModeButton"
+import styles from "./footer.module.scss"
 
 export default function Footer({ accounts, onThemeButtonClick, darkTheme }) {
   return (
     <footer id="footer" className={utils.footer}>
-      <button className="transition-transform hover:scale-110 active:scale-90"
-              onClick={() => onThemeButtonClick()}>{darkTheme ? <Sun/> : <Moon/>}</button>
+      <DarkModeButton onButtonClick={onThemeButtonClick} darkTheme={darkTheme} className={styles.darkModeButton}/>
       <div className="flex gap-2">
         {accounts.map(account => (
           <a key={account.name} href={account.url} rel="noreferrer" target="_blank">
