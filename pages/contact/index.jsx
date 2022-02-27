18d80e58084ba0347f3bcc2c4a2adaf7421c525a
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-27 16:16:51 +0100

    Updated contact page

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,9 +1,9 @@
 import utils from "../../styles/utils.module.scss"
 import Layout from "../../components/layout/layout/layout"
 import layoutData from "../../content/config.json"
-import styles from "./contact.module.scss"
 import { getPage } from "../../lib/services/pageService"
 import MdContent from "../../components/mdContent/mdContent"
+import ContactFormNetlify from "../../components/contactForm/contactFormNetlify"
 
 export const getStaticProps = async () => {
 
@@ -20,35 +20,8 @@ export const getStaticProps = async () => {
 const Contact = ({ aboutContent }) => {
   return (
     <div className={utils.page}>
-        <MdContent content={aboutContent} withSpacing />
-        <form
-          name="contact"
-          method="POST"
-          data-netlify="true"
-          className={styles.form}>
-          <input type="hidden" name="form-name" value="contact"/>
-          <p>
-            <label>
-              <span>Your Name:</span>
-              <input type="text" name="name" required />
-            </label>
-          </p>
-          <p>
-            <label>
-              <span>Your Email:</span>
-              <input type="email" name="email" required />
-            </label>
-          </p>
-          <p>
-            <label className="items-start">
-              <span>Your Message to me:</span>
-              <textarea name="message" />
-            </label>
-          </p>
-          <p>
-            <button type="submit">Send</button>
-          </p>
-        </form>
+      <MdContent content={aboutContent} withSpacing/>
+      <ContactFormNetlify/>
     </div>
   )
 }
