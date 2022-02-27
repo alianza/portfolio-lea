9c5372d0e311a4d94341cb5ff44a8f75bd60d88a
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-27 16:16:33 +0100

    Added svg assets

---
+++ components/svg/close.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Close = () => {
+  return(
+    <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 28 28" className="fill-secondary">
+      <path d="M28 2.82L25.18 0L14 11.18L2.82 0L0 2.82L11.18 14L0 25.18L2.82 28L14 16.82L25.18 28L28 25.18L16.82 14L28 2.82Z"/>
+    </svg>
+  )
+}
+
+export default Close
---
+++ components/svg/hamburger.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Hamburger = () => {
+  return(
+    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="28" viewBox="0 0 32 28" className="fill-secondary">
+      <path d="M0 28H32V23.3333H0V28ZM0 16.3333H32V11.6667H0V16.3333ZM0 0V4.66667H32V0H0Z"/>
+    </svg>
+  )
+}
+
+export default Hamburger
