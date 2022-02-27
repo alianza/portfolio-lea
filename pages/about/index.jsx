c8e2e4d8cf7440ecba1cb3090564df78f9db0ce3
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-27 02:02:26 +0100

    Updated about page

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -2,7 +2,7 @@ import utils from "../../styles/utils.module.scss"
 import Layout from "../../components/layout/layout/layout"
 import layoutData from "../../content/config.json"
 import { getPage } from "../../lib/services/pageService"
-import MDContent from "../../components/mdContent/MDContent"
+import MdContent from "../../components/mdContent/mdContent"
 
 export const getStaticProps = async () => {
   const aboutPage = getPage("about")
@@ -18,7 +18,7 @@ export const getStaticProps = async () => {
 const About = ({ aboutPage }) => {
   return (
     <div className={utils.page}>
-      <MDContent content={aboutPage} noDate/>
+      <MdContent content={aboutPage} noDate/>
     </div>
   )
 }
