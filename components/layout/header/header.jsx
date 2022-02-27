54deb27d93c497a7b4cf6441c99db85bbd442146
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-27 16:15:55 +0100

    Updated header

--- components/layout/header/header.jsx
+++ components/layout/header/header.jsx
@@ -1,18 +1,32 @@
-import React from "react"
+import React, { useEffect } from "react"
 import utils from '../../../styles/utils.module.scss'
 import NavLink from "../../navLink/navLink"
+import styles from "./header.module.scss"
+import DarkModeButton from "../../darkModeButton/darkModeButton"
+import Hamburger from "../../svg/hamburger"
+import Close from "../../svg/close"
 
-export default function Header({ title }) {
+export default function Header({ title, onThemeButtonClick, darkTheme }) {
+  const [isActive, setIsActive] = React.useState(false)
+
+  useEffect(() => {
+    isActive ? document.body.classList.add('no-scroll') : document.body.classList.remove('no-scroll')
+  }, [isActive])
+
   return (
     <header id="header" className={utils.header}>
-      <h1 className={utils.title}>{title}</h1>
-      <div className="flex gap-2">
+      <div className={utils.controls}>
+        <h1 className={utils.title}>{title}</h1>
+        <button onClick={() => setIsActive(!isActive)} className={`transition-transform hover:scale-110 active:scale-95 ${utils.menuButton}`}>{isActive ? <Close/> : <Hamburger/>}</button>
+      </div>
+      <nav onClick={() => setIsActive(false)} className={`${styles.navigation} ${isActive ? styles.active : ''}`}>
         <NavLink label="Home" href="/" />
         <NavLink label="About" href="/about" />
         <NavLink label="Portfolio" href="/portfolio" />
         <NavLink label="Articles" href="/articles" />
         <NavLink label="Contact" href="/contact" />
-      </div>
+        <DarkModeButton onButtonClick={onThemeButtonClick} darkTheme={darkTheme} className={styles.darkModeButton}/>
+      </nav>
     </header>
     )
 }
