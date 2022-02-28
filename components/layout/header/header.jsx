406855fc9c6abfe337a581c930fcb50100bb39be
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-28 20:43:35 +0100

    Fixed header darkMode transition & mobile close behavior

--- components/layout/header/header.jsx
+++ components/layout/header/header.jsx
@@ -19,12 +19,12 @@ export default function Header({ title, onThemeButtonClick, darkTheme }) {
         <h1 className={utils.title}>{title}</h1>
         <button onClick={() => setIsActive(!isActive)} className={`transition-transform hover:scale-110 active:scale-95 ${utils.menuButton}`}>{isActive ? <Close/> : <Hamburger/>}</button>
       </div>
-      <nav onClick={() => setIsActive(false)} className={`${styles.navigation} ${isActive ? styles.active : ''}`}>
-        <NavLink label="Home" href="/" />
-        <NavLink label="About" href="/about" />
-        <NavLink label="Portfolio" href="/portfolio" />
-        <NavLink label="Articles" href="/articles" />
-        <NavLink label="Contact" href="/contact" />
+      <nav className={`${styles.navigation} ${isActive ? styles.active : ''}`}>
+        <NavLink label="Home" href="/" onClick={() => setIsActive(false)} />
+        <NavLink label="About" href="/about" onClick={() => setIsActive(false)} />
+        <NavLink label="Portfolio" href="/portfolio" onClick={() => setIsActive(false)} />
+        <NavLink label="Articles" href="/articles" onClick={() => setIsActive(false)} />
+        <NavLink label="Contact" href="/contact" onClick={() => setIsActive(false)} />
         <DarkModeButton onButtonClick={onThemeButtonClick} darkTheme={darkTheme} className={styles.darkModeButton}/>
       </nav>
     </header>
--- components/navLink/navLink.jsx
+++ components/navLink/navLink.jsx
@@ -3,15 +3,20 @@ import Link from 'next/link'
 import { useRouter } from 'next/router'
 import utils from '../../styles/utils.module.scss'
 
-const NavLink = ({ label, href, pathName }) => {
+const NavLink = ({ label, href, pathName, onClick }) => {
   const router = useRouter()
 
   !pathName && (pathName = href)
 
   return (
-    <>
-      <Link href={href}><a className={`no-underline hover:font-bold ${ router.pathname === pathName ? utils.active : "" }`}>{label}</a></Link>
-    </>
+    <Link href={href}>
+      <a
+        onClick={onClick}
+        className={`no-underline hover:font-bold ${router.pathname === pathName ? utils.active : ""}`}>
+        {label}
+      </a
+      >
+    </Link>
   )
 }
 
