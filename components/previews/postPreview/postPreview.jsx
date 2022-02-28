624ea3ad775bd25352934e6138fc8132d7717293
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-28 21:29:20 +0100

    Fixed article and post previews images

--- components/previews/postPreview/postPreview.jsx
+++ components/previews/postPreview/postPreview.jsx
@@ -1,11 +1,11 @@
 import Link from "next/link"
 
-const PostPreview = ({ id, post }) => {
+const PostPreview = ({ post }) => {
   return (
     <div className="flex gap-4 items-center">
-      <Link href={`/portfolio/${id}`}><img alt="post thumbnail" src={post.data.thumbnail} className="w-48 h-48 object-cover m-0 cursor-pointer"/></Link>
-      <div className="flex flex-col justify-center">
-        <Link href={`/portfolio/${id}`}><a className="text-2xl block">{post.data.title}</a></Link>
+      <Link href={`/portfolio/${post.id}`}><a className="basis-[34%]"><img alt="post thumbnail" src={post.data.thumbnail} className="w-full object-cover aspect-video m-0"/></a></Link>
+      <div className="basis-[66%] flex flex-col justify-center">
+        <Link href={`/portfolio/${post.id}`}><a className="text-2xl block">{post.data.title}</a></Link>
         <time className="text-lg">{post.data.date}</time>
         <p className="max-h-24 overflow-ellipsis overflow-auto min-w-0">{post.data.description}</p>
       </div>
