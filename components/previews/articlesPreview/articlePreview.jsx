8839176bb2cf8abd696f1c9ec5457c331b7f322f
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-02-28 22:02:20 +0100

    Improved article and post previews

--- components/previews/articlesPreview/articlePreview.jsx
+++ components/previews/articlesPreview/articlePreview.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const ArticlePreview = ({ article }) => {
   return (
-    <div key={article.title} className="flex gap-4 items-center">
-      <a className="basis-[34%]" href={article.link} target="_blank" rel="noreferrer"><img src={article.image} alt={article.title} className="w-full aspect-video object-cover m-0"/></a>
-      <div className="basis-[66%] flex flex-col justify-center">
+    <div key={article.title} className="flex flex-col gap-2 items-center tablet:gap-4 tablet:flex-row">
+      <a className="tablet:w-1/3 shrink-0" href={article.link} target="_blank" rel="noreferrer"><img width="100%" height="100%" src={article.image} alt={article.title} className="w-full object-cover aspect-video m-0"/></a>
+      <div className="grow flex flex-col justify-center">
         <a href={article.link} className="text-2xl block" target="_blank" rel="noreferrer">{article.title}</a>
         <p className="text-lg">{article.date}</p>
         <p className="max-h-24 overflow-ellipsis overflow-auto min-w-0" dangerouslySetInnerHTML={{ __html: article.description }}/>
--- components/previews/postPreview/postPreview.jsx
+++ components/previews/postPreview/postPreview.jsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 const PostPreview = ({ post }) => {
   return (
-    <div className="flex gap-4 items-center">
-      <Link href={`/portfolio/${post.id}`}><a className="basis-[34%]"><img alt="post thumbnail" src={post.data.thumbnail} className="w-full object-cover aspect-video m-0"/></a></Link>
-      <div className="basis-[66%] flex flex-col justify-center">
+    <div className="flex flex-col gap-2 items-center tablet:gap-4 tablet:flex-row">
+      <Link href={`/portfolio/${post.id}`}><a className="tablet:w-1/3 shrink-0"><img alt="post thumbnail" src={post.data.thumbnail} className="w-full object-cover aspect-video m-0"/></a></Link>
+      <div className="flex flex-col justify-center">
         <Link href={`/portfolio/${post.id}`}><a className="text-2xl block">{post.data.title}</a></Link>
         <time className="text-lg">{post.data.date}</time>
         <p className="max-h-24 overflow-ellipsis overflow-auto min-w-0">{post.data.description}</p>
