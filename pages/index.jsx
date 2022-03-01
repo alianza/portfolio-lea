78d2831080b92eee41cb27558a36854d8ea05ad2
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-02 00:27:22 +0100

    Added homepage content

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,18 +1,29 @@
 import Head from 'next/head'
-import { useEffect } from "react"
+import React, { useEffect } from "react"
 import utils from "../styles/utils.module.scss"
 import Layout from "../components/layout/layout/layout"
 import layoutData from "../content/config.json"
+import { getPosts } from "../lib/services/postsService"
+import { getArticles } from "../lib/services/mediumService"
+import PostHomePreview from "../components/previews/home/postPreview/postsHomePreview"
+import ArticleHomePreview from "../components/previews/home/articlesPreview/articlesHomePreview"
 
 export const getStaticProps = async () => {
+
+  const posts = getPosts()
+
+  const articles = await getArticles(layoutData.username_medium)
+
   return {
     props: {
+      posts,
+      articles,
       layoutData
     },
   }
 }
 
-const Home = () => {
+const Home = ({ posts, articles: { dataMedium } }) => {
   useEffect(() => {
     if (window.netlifyIdentity) {
       window.netlifyIdentity.on("init", user => {
@@ -26,13 +37,13 @@ const Home = () => {
   }, []);
 
   return (
-    <div className={`${utils.page} flex items-center justify-center flex-col min-h-full`}>
-        <h1 className="text-6xl font-bold text-center">
-         Homepage{" "}
-          <span className="text-blue-600">
-            Content!
-          </span>
-        </h1>
+    <div className={`${utils.page} flex flex-col gap-12`}>
+      <div className="flex flex-col gap-2 items-center items-stretch tablet:flex-row">
+        {posts.map((post) => <PostHomePreview key={post.id} post={post}/> )}
+      </div>
+      <div className="flex flex-col gap-2 items-center items-stretch tablet:flex-row">
+        {dataMedium.map((article) => <ArticleHomePreview key={article.title} article={article}/> )}
+      </div>
     </div>
   )
 }
