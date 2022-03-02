3abf6328a02a737223af4c06308202ed0ee37a4d
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-03 00:05:52 +0100

    Change header naming and nav order

--- components/layout/header/header.jsx
+++ components/layout/header/header.jsx
@@ -8,24 +8,26 @@ import Close from "../../svg/close"
 import Link from "next/link"
 
 export default function Header({ title, onThemeButtonClick, darkTheme }) {
-  const [isActive, setIsActive] = React.useState(false)
+  const [menuOpen, setMenuOpen] = React.useState(false)
 
   useEffect(() => {
-    isActive ? document.body.classList.add('no-scroll') : document.body.classList.remove('no-scroll')
-  }, [isActive])
+    menuOpen ? document.body.classList.add('no-scroll') : document.body.classList.remove('no-scroll')
+  }, [menuOpen])
 
   return (
     <header id="header" className={utils.header}>
       <div className={utils.controls}>
         <Link href="/"><a className="no-underline"><h1 className={utils.title}>{title}</h1></a></Link>
-        <button onClick={() => setIsActive(!isActive)} className={`transition-transform hover:scale-110 active:scale-95 ${utils.menuButton}`}>{isActive ? <Close/> : <Hamburger/>}</button>
+        <button onClick={() => setMenuOpen(!menuOpen)} className={`transition-transform hover:scale-110 active:scale-95 ${utils.menuButton}`}>
+          { menuOpen ? <Close/> : <Hamburger/> }
+        </button>
       </div>
-      <nav className={`${styles.navigation} ${isActive ? styles.active : ''}`}>
-        <NavLink label="Home" href="/" onClick={() => setIsActive(false)} />
-        <NavLink label="About" href="/about" onClick={() => setIsActive(false)} />
-        <NavLink label="Portfolio" href="/portfolio" onClick={() => setIsActive(false)} />
-        <NavLink label="Articles" href="/articles" onClick={() => setIsActive(false)} />
-        <NavLink label="Contact" href="/contact" onClick={() => setIsActive(false)} />
+      <nav className={`${styles.navigation} ${menuOpen ? styles.active : ''}`}>
+        <NavLink label="Home" href="/" onClick={() => setMenuOpen(false)} />
+        <NavLink label="About" href="/about" onClick={() => setMenuOpen(false)} />
+        <NavLink label="Articles" href="/articles" onClick={() => setMenuOpen(false)} />
+        <NavLink label="Portfolio" href="/portfolio" onClick={() => setMenuOpen(false)} />
+        <NavLink label="Contact" href="/contact" onClick={() => setMenuOpen(false)} />
         <DarkModeButton onButtonClick={onThemeButtonClick} darkTheme={darkTheme} className={styles.darkModeButton}/>
       </nav>
     </header>
