ecbe36e815375d4fffd959b056e4ef0599d90578
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-02 23:56:38 +0100

    Updated homepage to show most recent 3 posts and articles

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,4 @@
-import Head from 'next/head'
-import React, { useEffect } from "react"
+import React from "react"
 import utils from "../styles/utils.module.scss"
 import Layout from "../components/layout/layout/layout"
 import layoutData from "../content/config.json"
@@ -7,6 +6,9 @@ import { getPosts } from "../lib/services/postsService"
 import { getArticles } from "../lib/services/mediumService"
 import PostHomePreview from "../components/previews/home/postPreview/postsHomePreview"
 import ArticleHomePreview from "../components/previews/home/articlesPreview/articlesHomePreview"
+import { useNetlifyIdentityRedirectHook } from "../lib/eventListeners"
+import { getPage } from "../lib/services/pageService"
+import HomePreviewCollection from "../components/previews/home/previewCollections/homePreviewCollection"
 
 export const getStaticProps = async () => {
 
@@ -14,8 +16,11 @@ export const getStaticProps = async () => {
 
   const articles = await getArticles(layoutData.username_medium)
 
+  const homeContent = getPage("home")
+
   return {
     props: {
+      homeContent,
       posts,
       articles,
       layoutData
@@ -23,27 +28,23 @@ export const getStaticProps = async () => {
   }
 }
 
-const Home = ({ posts, articles: { dataMedium } }) => {
-  useEffect(() => {
-    if (window.netlifyIdentity) {
-      window.netlifyIdentity.on("init", user => {
-        if (!user) {
-          window.netlifyIdentity.on("login", () => {
-            document.location.href = "/admin/";
-          });
-        }
-      })
-    }
-  }, []);
+const Home = ({ homeContent, posts, articles: { dataMedium } }) => {
+  useNetlifyIdentityRedirectHook()
 
   return (
     <div className={`${utils.page} flex flex-col gap-12`}>
-      <div className="flex flex-col gap-2 items-center items-stretch tablet:flex-row">
-        {posts.map((post) => <PostHomePreview key={post.id} post={post}/> )}
-      </div>
-      <div className="flex flex-col gap-2 items-center items-stretch tablet:flex-row">
-        {dataMedium.map((article) => <ArticleHomePreview key={article.title} article={article}/> )}
-      </div>
+      <HomePreviewCollection
+        title={homeContent.postsTitle}
+        label={homeContent.postsLabel}
+        link="/portfolio"
+        content={posts.map((post) => <PostHomePreview key={post.id} post={post}/>)}
+      />
+      <HomePreviewCollection
+        title={homeContent.articlesTitle}
+        label={homeContent.articlesLabel}
+        link="/articles"
+        content={dataMedium.map((article) => <ArticleHomePreview key={article.title} article={article}/>)}
+      />
     </div>
   )
 }
