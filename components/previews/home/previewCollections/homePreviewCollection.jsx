986e0e98150f37ca70d986c4d6ca64eb1feeafbe
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-02 23:55:53 +0100

    Added homePreviewCollection.jsx component

---
+++ components/previews/home/previewCollections/homePreviewCollection.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link"
+import * as PropTypes from "prop-types"
+import React from "react"
+import styles from "./previewCollections.module.scss"
+
+function HomePreviewCollection({ title, label, link, content }) {
+  return(
+  <div className={styles.container}>
+    <div className={styles.heading}>
+      <h1 className={styles.headingTitle}>{title}</h1>
+      <Link href={link}>
+        <a className={`${styles.link} group hidden mobile:flex`}>
+          <span className={styles.label}>{label}</span>
+          <span className={styles.arrow}>→</span>
+        </a>
+      </Link>
+    </div>
+    <div className={styles.contentWrapper}>
+      {content}
+    </div>
+    <Link href={link}>
+      <a className={`${styles.link} mt-4 self-end group inline-flex mobile:hidden`}>
+        <span className={styles.label}>{label}</span>
+        <span className={styles.arrow}>→</span>
+      </a>
+    </Link>
+  </div>
+  )
+}
+
+HomePreviewCollection.propTypes = {
+  title: PropTypes.string,
+  label: PropTypes.string,
+  link: PropTypes.string,
+  content: PropTypes.array
+}
+
+export default HomePreviewCollection
