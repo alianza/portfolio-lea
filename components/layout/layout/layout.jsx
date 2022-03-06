77d9caf6ba451a95820748c4a200fa8e37989743
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-06 03:48:31 +0100

    Updated site config

--- components/layout/layout/layout.jsx
+++ components/layout/layout/layout.jsx
@@ -10,7 +10,7 @@ import utils from "../../../styles/utils.module.scss"
 
 const darkThemeKey = 'darkTheme'
 
-export default function Layout({ site_title, site_description, accounts, children}) {
+export default function Layout({ site_heading, site_title, site_description, accounts, children}) {
     const [darkTheme, setDarkTheme] = useState(false)
 
     useDarkThemeListener(setDarkTheme)
@@ -26,21 +26,20 @@ export default function Layout({ site_title, site_description, accounts, childre
     return (
     <div id="app">
         <Head>
-            <title>{site_description}</title>
+            <title>{site_title}</title>
             <link rel="icon" href="/favicon.ico"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
-            <meta name="description" content="Team Rockstars IT Tech Case statically rendered with Next.js"/>
-            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-            <meta name="msapplication-TileColor" content="#da532c"/>
-            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>
-            <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' />
-            <link rel="manifest" href="/manifest.json"/>
+            <meta name="description" content={site_description}/>
+            {/*<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>*/}
+            {/*<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>*/}
+            {/*<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>*/}
+            {/*<meta name="msapplication-TileColor" content="#da532c"/>*/}
+            {/*<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>*/}
+            {/*<link rel="manifest" href="/manifest.json"/>*/}
             <meta name="theme-color" content={darkTheme ? '#000' : '#fff'}/>
         </Head>
 
-        <Header title={site_title} darkTheme={darkTheme} onThemeButtonClick={toggleTheme} />
+        <Header title={site_heading} darkTheme={darkTheme} onThemeButtonClick={toggleTheme} />
 
         <main id="content" className={utils.content}>{children}</main>
 
