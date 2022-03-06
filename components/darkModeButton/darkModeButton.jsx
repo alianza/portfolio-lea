15770a339448255c6dd482ffbab1d9525469fc8c
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-06 01:00:39 +0100

    Updated darkMode button to support white moon setting

--- components/darkModeButton/darkModeButton.jsx
+++ components/darkModeButton/darkModeButton.jsx
@@ -2,11 +2,11 @@ import Sun from "../svg/sun"
 import Moon from "../svg/moon"
 import React from "react"
 
-const DarkModeButton = ({ onButtonClick, darkTheme, className }) => {
+const DarkModeButton = ({ onButtonClick, darkTheme, className, whiteMoon }) => {
   return(
-    <button style={{filter: `drop-shadow(0px 0px 1px ${darkTheme ? 'white' : 'black'})`}}
+    <button style={{filter: `drop-shadow(0px 0px 1px white)`}}
             className={`transition-transform hover:scale-110 active:scale-95 ${className ? className : ''}`}
-            onClick={() => onButtonClick()}>{darkTheme ? <Sun white/> : <Moon white/>}</button>
+            onClick={() => onButtonClick()}>{darkTheme ? <Sun/> : <Moon color={whiteMoon ? 'white' : ''}/>}</button>
   )
 }
 
