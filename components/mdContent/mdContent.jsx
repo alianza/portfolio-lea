8464c4166cbf1bcf17074591a1f43b0215f91b8c
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-06 01:53:47 +0100

    Updated mdContent.jsx component to use CategoryLabel component

--- components/mdContent/mdContent.jsx
+++ components/mdContent/mdContent.jsx
@@ -1,22 +1,22 @@
 import React from "react"
 import contentStyles from "./mdContent.module.scss"
-import Head from "next/head"
+import CategoryLabel from "../categoryLabel/categoryLabel"
 
-const MdContent = ({ content: { title, date, thumbnail, category, content }, noDate, withSpacing } ) => {
+const MdContent = ({ content: { title, date, thumbnail, category, content }, noDate, withSpacing }) => {
   return (
     <>
-    <article className={contentStyles.content}>
-      <h1 className={contentStyles.mainTitle}>{title}</h1>
-      <div className={contentStyles.metaData}>
-        {!noDate && <time className="m-0">{date}</time>}
-        {category && <span className={contentStyles.categoryLabel}>{category}</span>}
-      </div>
-      {thumbnail && <img className="w-full" alt='thumbnail' src={thumbnail}/>}
-      <div className="relative z-[1]" dangerouslySetInnerHTML={{ __html: content }}/>
-    </article>
+      <article className={contentStyles.content}>
+        <h1 className={contentStyles.mainTitle}>{title}</h1>
+        <div className={contentStyles.metaData}>
+          {!noDate && <time className="m-0">{date}</time>}
+          {category && <CategoryLabel category={category}/>}
+        </div>
+        {thumbnail && <img className="w-full" alt="thumbnail" src={thumbnail}/>}
+        <div className="relative z-[1]" dangerouslySetInnerHTML={{ __html: content }}/>
+      </article>
       {withSpacing && <hr className="my-4"/>}
     </>
   )
 }
 
-export default MdContent;
+export default MdContent
