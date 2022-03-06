32db98f019bc87f8f2c24eff80451596145522e5
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-06 15:08:34 +0100

    isr test

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,26 +2,24 @@ import React from "react"
 import utils from "../styles/utils.module.scss"
 import Layout from "../components/layout/layout/layout"
 import layoutData from "../content/config.json"
-import { getPosts } from "../lib/services/postsService"
 import { getArticles } from "../lib/services/mediumService"
 import PostHomePreview from "../components/previews/home/postPreview/postsHomePreview"
 import ArticleHomePreview from "../components/previews/home/articlesPreview/articlesHomePreview"
 import { useNetlifyIdentityRedirectHook } from "../lib/eventListeners"
-import { getPage } from "../lib/services/pageService"
 import HomePreviewCollection from "../components/previews/home/previewCollections/homePreviewCollection"
 
 export const getStaticProps = async () => {
 
-  const posts = getPosts().slice(0, 3)
+  // const posts = getPosts().slice(0, 3)
 
   const articles = await getArticles(layoutData.username_medium)
 
-  const homeContent = getPage("home")
+  // const homeContent = getPage("home")
 
   return {
     props: {
-      homeContent,
-      posts,
+      // homeContent,
+      // posts,
       articles: articles.dataMedium.slice(0, 3),
       layoutData
     },
@@ -35,18 +33,18 @@ const Home = ({ homeContent, posts, articles }) => {
   return (
     <div className={`${utils.page} flex flex-col gap-12`}>
       <HomePreviewCollection
-        title={homeContent.articlesTitle}
-        label={homeContent.articlesLabel}
+        title={"Articles"}
+        label={"Articles"}
         link="/articles"
         content={articles.map((article) => <ArticleHomePreview key={article.title} article={article}/>)}
       />
       <hr className="-mb-4 -mt-10 myblock mobile:hidden"/>
-      <HomePreviewCollection
-        title={homeContent.postsTitle}
-        label={homeContent.postsLabel}
-        link="/portfolio"
-        content={posts.map((post) => <PostHomePreview key={post.id} post={post}/>)}
-      />
+      {/*<HomePreviewCollection*/}
+      {/*  title={"Posts"}*/}
+      {/*  label={"Posts"}*/}
+      {/*  link="/portfolio"*/}
+      {/*  content={posts.map((post) => <PostHomePreview key={post.id} post={post}/>)}*/}
+      {/*/>*/}
     </div>
   )
 }
