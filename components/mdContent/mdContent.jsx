04534caa18a6f4154c6a3c86cdbe6be81df07f3d
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-08 20:08:55 +0100

    Removed hr from mdContent component

--- components/mdContent/mdContent.jsx
+++ components/mdContent/mdContent.jsx
@@ -2,20 +2,17 @@ import React from "react"
 import contentStyles from "./mdContent.module.scss"
 import CategoryLabel from "../categoryLabel/categoryLabel"
 
-const MdContent = ({ content: { title, date, thumbnail, category, content }, noDate, withSpacing }) => {
+const MdContent = ({ content: { title, date, thumbnail, category, content }, noDate }) => {
   return (
-    <>
-      <article className={contentStyles.content}>
-        <h1 className={contentStyles.mainTitle}>{title}</h1>
-        <div className={contentStyles.metaData}>
-          {!noDate && <time className="m-0">{date}</time>}
-          {category && <CategoryLabel category={category}/>}
-        </div>
-        {thumbnail && <img className="w-full" alt="thumbnail" src={thumbnail}/>}
-        <div className="relative z-[1]" dangerouslySetInnerHTML={{ __html: content }}/>
-      </article>
-      {withSpacing && <hr className="my-4"/>}
-    </>
+    <article className={`${contentStyles.content}`}>
+      <h1 className={contentStyles.mainTitle}>{title}</h1>
+      <div className={contentStyles.metaData}>
+        {!noDate && <time className="m-0">{date}</time>}
+        {category && <CategoryLabel category={category}/>}
+      </div>
+      {thumbnail && <img className="w-full" alt="thumbnail" src={thumbnail}/>}
+      <div className="relative z-[1]" dangerouslySetInnerHTML={{ __html: content }}/>
+    </article>
   )
 }
 
