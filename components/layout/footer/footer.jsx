0e152d0929bcfdcb5a3333c75703e7468f801a05
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-08 19:26:06 +0100

    Added Next Image component to footer social account images

--- components/layout/footer/footer.jsx
+++ components/layout/footer/footer.jsx
@@ -2,6 +2,7 @@ import React from "react"
 import utils from '../../../styles/utils.module.scss'
 import DarkModeButton from "../../darkModeButton/darkModeButton"
 import styles from "./footer.module.scss"
+import Image from 'next/image'
 
 export default function Footer({ accounts, onThemeButtonClick, darkTheme }) {
   return (
@@ -12,8 +13,8 @@ export default function Footer({ accounts, onThemeButtonClick, darkTheme }) {
       </div>
       <div className="flex gap-4">
         {accounts.map(account => (
-          <a className="w-8 transition-transform hover:scale-110 active:scale-95" key={account.name} title={account.name} href={account.url} rel="noreferrer" target="_blank">
-            <img alt={account.name} className="rounded" src={account.icon} />
+          <a className={`relative w-8 h-8 ${utils.hoverEffectStrong}`} key={account.name} title={account.name} href={account.url} rel="noreferrer" target="_blank">
+            <Image layout="fill" alt={account.name} className="rounded" src={account.icon} />
           </a>
         ))}
       </div>
