39a6102f9da67e9228fdeeaf16b0697edccec7cc
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-08 19:27:35 +0100

    Use hover styles in darkModeButton component

--- components/darkModeButton/darkModeButton.jsx
+++ components/darkModeButton/darkModeButton.jsx
@@ -1,12 +1,15 @@
 import Sun from "../svg/sun"
 import Moon from "../svg/moon"
 import React from "react"
+import utils from "../../styles/utils.module.scss"
 
 const DarkModeButton = ({ onButtonClick, darkTheme, className, whiteMoon }) => {
   return(
-    <button style={{filter: `drop-shadow(0px 0px 1px white)`}}
-            className={`transition-transform hover:scale-110 active:scale-95 ${className ? className : ''}`}
-            onClick={() => onButtonClick()}>{darkTheme ? <Sun/> : <Moon color={whiteMoon ? 'white' : ''}/>}</button>
+    <button
+      style={{ filter: `drop-shadow(0px 0px 1px white)` }}
+      className={`${utils.hoverEffectStrong} ${className ? className : ''}`}
+      onClick={() => onButtonClick()}>{darkTheme ? <Sun/> : <Moon color={whiteMoon ? 'white' : ''}/>}
+    </button>
   )
 }
 
