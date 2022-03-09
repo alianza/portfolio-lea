28be75361d92aa1246f84d82f009e2a4a977b9d2
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-09 11:23:56 +0100

    Fixed header and footer dark mode buttons

--- components/darkModeButton/darkModeButton.jsx
+++ components/darkModeButton/darkModeButton.jsx
@@ -1,14 +1,12 @@
 import Sun from "../svg/sun"
 import Moon from "../svg/moon"
 import React from "react"
-import utils from "../../styles/utils.module.scss"
 
-const DarkModeButton = ({ onButtonClick, darkTheme, className, whiteMoon }) => {
+const DarkModeButton = ({ onButtonClick, darkTheme, className }) => {
   return(
     <button
-      style={{ filter: `drop-shadow(0px 0px 1px white)` }}
-      className={`transition-transform hover:scale-110 active:scale-95 ${className ? className : ''}`}
-      onClick={() => onButtonClick()}>{darkTheme ? <Sun/> : <Moon color={whiteMoon ? 'white' : ''}/>}
+      className={`transition-transform hover:scale-110 active:scale-95 drop-shadow-[0_0_1px_var(--accent-9)] ${className ? className : ''}`}
+      onClick={() => onButtonClick()}>{darkTheme ? <Sun/> : <Moon/>}
     </button>
   )
 }
--- components/layout/footer/footer.jsx
+++ components/layout/footer/footer.jsx
@@ -8,12 +8,12 @@ export default function Footer({ accounts, onThemeButtonClick, darkTheme }) {
   return (
     <footer id="footer" className={utils.footer}>
       <div className="flex gap-8 items-center">
-        <DarkModeButton onButtonClick={onThemeButtonClick} darkTheme={darkTheme} className={styles.darkModeButton} whiteMoon/>
-        <span className="text-text-light text-center mobile:text-left mobile:font-bold text-sm xs:text-base">Copyright Léa Shamaa © All rights reserved.</span>
+        <DarkModeButton onButtonClick={onThemeButtonClick} darkTheme={darkTheme} className={styles.darkModeButton}/>
+        <span className="text-text-primary text-center mobile:text-left text-sm xs:text-base">Copyright Léa Shamaa © All rights reserved.</span>
       </div>
       <div className="flex gap-4">
         {accounts.map(account => (
-          <a className={`relative w-8 h-8 ${utils.hoverEffectStrong}`} key={account.name} title={account.name} href={account.url} rel="noreferrer" target="_blank">
+          <a className="relative w-8 h-8 transition-transform hover:scale-110 active:scale-95" key={account.name} title={account.name} href={account.url} rel="noreferrer" target="_blank">
             <Image layout="fill" alt={account.name} className="rounded" src={account.icon} />
           </a>
         ))}
