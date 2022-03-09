95355acd6a2c74497f8deceb39d52fd495aa77ab
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-09 20:17:23 +0100

    Test

--- pages/api/imageProxy.js
+++ pages/api/imageProxy.js
@@ -1,5 +1,4 @@
 import { withImageProxy } from '@blazity/next-image-proxy'
 
 // https://github.com/Blazity/next-image-proxy
-
 export default withImageProxy({ whitelistedPatterns: [/^https?:\/\/(.*).medium.com/] })
