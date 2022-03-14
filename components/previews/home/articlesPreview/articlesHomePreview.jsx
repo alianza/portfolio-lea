026044724532cce6b665321d5dcf2b46c4ecfde0
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-14 12:26:31 +0100

    Added next-image-proxy and next-image-proxy api route

--- components/previews/home/articlesPreview/articlesHomePreview.jsx
+++ components/previews/home/articlesPreview/articlesHomePreview.jsx
@@ -1,6 +1,7 @@
 import * as PropTypes from "prop-types"
 import React from "react"
 import styles from "../../preview.module.scss"
+import Image from "next/image"
 
 ArticleHomePreview.propTypes = { article: PropTypes.object.isRequired }
 
@@ -8,7 +9,7 @@ function ArticleHomePreview({ article }) {
   return (
     <div className={styles.previewHomeContainer}>
       <a href={article.link} target="_blank" rel="noreferrer" className={`${styles.imageLinkStyle} transition-transform hover:scale-[1.02] active:scale-[.98]`}>
-        <img alt={`${article.title} thumbnail`} className={styles.imageStyle} src={article.image}/>
+        <Image layout="fill" alt={`${article.title} thumbnail`} className={styles.imageStyle} src={`/api/imageProxy?imageUrl=${article.image}`}/>
       </a>
       <div className={styles.contentStyle}>
         <a href={article.link} target="_blank" rel="noreferrer" className={styles.title} dangerouslySetInnerHTML={{ __html: article.title }}/>
