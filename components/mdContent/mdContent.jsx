29df55dd83683bc59ba2e371210dd930a3307c9a
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-03-20 17:54:43 +0100

    Added custom hooks to MdContent

--- components/mdContent/mdContent.jsx
+++ components/mdContent/mdContent.jsx
@@ -1,10 +1,16 @@
 import React from "react"
 import contentStyles from "./mdContent.module.scss"
 import CategoryLabel from "../categoryLabel/categoryLabel"
+import { useDetailTagsAnimation, useInstagramEmbedScript } from "../../lib/markdownEnrichment"
 
 const MdContent = ({ content: { title, date, thumbnail, category, content }, noDate }) => {
+
+  useDetailTagsAnimation()
+
+  useInstagramEmbedScript(content)
+
   return (
-    <article className={`${contentStyles.content}`}>
+    <article className={contentStyles.content}>
       <h1 className={contentStyles.mainTitle}>{title}</h1>
       <div className={contentStyles.metaData}>
         {!noDate && <time className="m-0">{date}</time>}
