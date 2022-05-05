80aa94bad612e7cf02628d142dd4aba684fc095f
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-05-05 21:27:07 +0200

    Add offline fallback page

---
+++ pages/_offline.jsx
@@ -0,0 +1,7 @@
+export default () => (
+    <div>
+        <h1>Oops, your offline! 📡</h1>
+        <h2>Please reconnect the network to view my portfolio :)</h2>
+        <a href="/">Reload 🔁</a>
+    </div>
+)
