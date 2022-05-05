ff14c78c4d43fc8f7188b34cd69df44bd249abf0
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-05-05 21:05:05 +0200

    Added favicons

--- components/layout/layout/layout.jsx
+++ components/layout/layout/layout.jsx
@@ -30,12 +30,12 @@ export default function Layout({ site_heading, site_title, site_description, acc
             <link rel="icon" href="/favicon.ico"/>
             <meta name="viewport" content="width=device-width, initial-scale=1"/>
             <meta name="description" content={site_description}/>
-            {/*<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>*/}
-            {/*<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>*/}
-            {/*<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>*/}
-            {/*<meta name="msapplication-TileColor" content="#da532c"/>*/}
-            {/*<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5"/>*/}
-            {/*<link rel="manifest" href="/manifest.json"/>*/}
+            <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+            <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+            <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+            <link rel="manifest" href="/site.webmanifest"/>
+            <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#51a493"/>
+            <meta name="msapplication-TileColor" content="#da532c"/>
             <meta name="theme-color" content={darkTheme ? '#000' : '#fff'}/>
         </Head>
 
