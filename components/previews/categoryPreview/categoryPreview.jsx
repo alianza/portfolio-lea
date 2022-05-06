b03d62462e90366042cdc619e33c29e8e502ecfd
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-05-06 04:41:26 +0200

    Added categoryPreview.jsx component

---
+++ components/previews/categoryPreview/categoryPreview.jsx
@@ -0,0 +1,25 @@
+import utils from "../../../styles/utils.module.scss"
+import * as PropTypes from "prop-types"
+import React from "react"
+import Link from 'next/link'
+import SOrNot from "../../sorNot/sOrNot"
+
+function CategoryPreview({ category }) {
+  const textColor = category.textColor === "light" ? "var(--text-light)" : "var(--text-dark)"
+  const textStyle = category.textColor !== "auto" ? { color: textColor } : {}
+
+  return (
+    <Link href={`/blog/categories/${category.name}`}>
+      <a className={`p-4 relative cursor-pointer no-underline ${utils.hoverEffectSlight}`}
+         style={{ backgroundColor: category.color }}>
+        <h1 style={{ ...textStyle }} className="text-2xl">{category.name}</h1>
+        <span style={{ ...textStyle }}>{category.count} Post<SOrNot arrayLength={category.count}/></span>
+        <span className="absolute block tablet:hidden right-1 bottom-0 text-sm">Read more...</span>
+      </a>
+    </Link>
+  )
+}
+
+CategoryPreview.propTypes = { category: PropTypes.object.isRequired }
+
+export default CategoryPreview
