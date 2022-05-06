2bcd101f49ebcdc16dd7c0ca3f4ecd789a4a2cf2
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-05-06 04:39:34 +0200

    Added categories page and category page

---
+++ pages/blog/categories/[name].jsx
@@ -0,0 +1,51 @@
+import utils from "../../../styles/utils.module.scss"
+import Layout from "../../../components/layout/layout/layout"
+import layoutData from "../../../content/config.json"
+import { getPostsByCategory } from "../../../lib/services/postsService"
+import { getCategory } from "../../../lib/services/configService"
+import { getCategories } from "../../../lib/services/postsService"
+import styles from "../../../components/previews/preview.module.scss"
+import React from "react"
+import PostPreview from "../../../components/previews/postPreview/postPreview"
+import MdContent from "../../../components/mdContent/mdContent"
+
+export const getStaticPaths = async () => {
+
+  const categories = await getCategories()
+
+  return {
+    paths: categories.map(category => ({ params: { name: category.name } })),
+    fallback: false,
+  }
+}
+
+export const getStaticProps = async ({ params }) => {
+
+  const posts = await getPostsByCategory(params.name)
+
+  posts.map(async (post) => post.data.category = await getCategory(post.category) || null)
+
+  return {
+    props: {
+      posts,
+      category: params.name,
+      layoutData
+    },
+  }
+}
+
+const Category = ({ posts, category }) => {
+  return (
+    <div className={utils.page}>
+      <MdContent content={{ title: `Category: ${category}`, content: '' }} categoriesLink/>
+      <hr className="my-4"/>
+      <div className={styles.previewList}>
+        {posts.map((post) => <PostPreview key={post.id} post={post}/>)}
+      </div>
+    </div>
+  )
+}
+
+Category.withLayout = (page, layoutData) => <Layout {...layoutData}>{page}</Layout>
+
+export default Category
--- pages/blog/categories/index.jsx
+++ pages/blog/categories/index.jsx
@@ -1,31 +1,35 @@
 import utils from "../../../styles/utils.module.scss"
-import styles from "../../../components/previews/preview.module.scss"
 import { getCategories } from "../../../lib/services/postsService"
 import Layout from "../../../components/layout/layout/layout"
 import layoutData from "../../../content/config.json"
 import React from "react"
 import MdContent from "../../../components/mdContent/mdContent"
-import CategoryLabel from "../../../components/categoryLabel/categoryLabel"
+import CategoryPreview from "../../../components/previews/categoryPreview/categoryPreview"
+import { getPage } from "../../../lib/services/pageService"
 
 export const getStaticProps = async () => {
 
   const categories = await Promise.all(await getCategories())
 
+  const categoriesContent = await getPage("categories")
+
   return {
     props: {
       categories,
+      categoriesContent,
       layoutData
     }
   }
 }
 
-const Categories = ({ categories }) => {
+
+const Categories = ({ categories, categoriesContent }) => {
   return (
     <div className={utils.page}>
-      <MdContent content={{title: 'Categories', content: ''}}/>
+      <MdContent content={categoriesContent}/>
       <hr className="my-4"/>
-      <div className={styles.previewList}>
-        {categories.map((category) => <CategoryLabel key={category.name} category={category}/>)}
+      <div className="flex gap-2 flex-col tablet:flex-row">
+        {categories.map((category) => <CategoryPreview key={category.name} category={category}/>)}
       </div>
     </div>
   )
