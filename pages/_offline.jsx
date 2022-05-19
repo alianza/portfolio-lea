5d6d19cc0d8927f4fae98ee67a99f6313ca32335
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-05-19 15:19:57 +0200

    Updated offline fallback page

--- pages/_offline.jsx
+++ pages/_offline.jsx
@@ -1,7 +1,7 @@
 export default () => (
-    <div>
-        <h1>Oops, your offline! 📡</h1>
-        <h2>Please reconnect the network to view my portfolio :)</h2>
+    <div className="block text-center">
+        <h1 className="text-4xl">Oops, your offline! 📡</h1>
+        <h2 className="text-lg">Please reconnect the network to view my portfolio :)</h2>
         <a href="/">Reload 🔁</a>
     </div>
 )
