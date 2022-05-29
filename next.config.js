a2eba684fa4cbd2dba9eec1355297fbbfa09d4dd
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-05-29 21:26:35 +0200

    Build analyzer config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,19 @@
 const withPWA = require('next-pwa')
+const withPlugins = require('next-compose-plugins')
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
 
 const prod = process.env.NODE_ENV === 'production'
 
-module.exports = withPWA({
-  pwa: {
-    dest: 'public',
-    disable: !prod,
-    buildExcludes: [/middleware-manifest\.json$/]
-  }
-})
+module.exports = withPlugins([
+  [withPWA, {
+    pwa: {
+      dest: 'public',
+      disable: !prod,
+      buildExcludes: [/middleware-manifest\.json$/]
+    }
+  }],
+  [withBundleAnalyzer],
+])
