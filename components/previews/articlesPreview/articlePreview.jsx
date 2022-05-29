e521d447707ec03765b051bd996bc1c84d98ab16
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-05-29 21:27:12 +0200

    Move containers to AnimationOnScroll component

--- components/previews/articlesPreview/articlePreview.jsx
+++ components/previews/articlesPreview/articlePreview.jsx
@@ -7,16 +7,14 @@ ArticlePreview.propTypes = { article: PropTypes.object.isRequired }
 
 function ArticlePreview({ article }) {
   return (
-    <AnimationOnScroll animateIn="animate__fadeInUp" duration=".5" offset="0" animateOnce>
-      <div className={styles.previewContainer}>
-        <a href={article.link} className={`${styles.linkStyle} transition-transform hover:scale-[1.02] active:scale-[.98]`} target="_blank" rel="noreferrer">
-          <img alt={`${article.title} thumbnail`} className={styles.imageStyle} src={article.image}/>
-        </a>
-        <div className={styles.contentStyle}>
-          <a href={article.link} className="text-2xl" target="_blank" rel="noreferrer" dangerouslySetInnerHTML={{ __html: article.title }}/>
-          <p className="text-lg">{article.date}</p>
-          <p className={styles.description} dangerouslySetInnerHTML={{ __html: article.description }}/>
-        </div>
+    <AnimationOnScroll animateIn="animate__fadeInUp" duration=".5" offset="0" animateOnce className={styles.previewContainer}>
+      <a href={article.link} className={`${styles.linkStyle} transition-transform hover:scale-[1.02] active:scale-[.98]`} target="_blank" rel="noreferrer">
+        <img alt={`${article.title} thumbnail`} className={styles.imageStyle} src={article.image}/>
+      </a>
+      <div className={styles.contentStyle}>
+        <a href={article.link} className="text-2xl" target="_blank" rel="noreferrer" dangerouslySetInnerHTML={{ __html: article.title }}/>
+        <p className="text-lg">{article.date}</p>
+        <p className={styles.description} dangerouslySetInnerHTML={{ __html: article.description }}/>
       </div>
     </AnimationOnScroll>
   )
--- components/previews/experiencePreview/experiencePreview.jsx
+++ components/previews/experiencePreview/experiencePreview.jsx
@@ -9,27 +9,25 @@ ExperiencePreview.propTypes = { experience: PropTypes.object.isRequired }
 
 function ExperiencePreview({ experience }) {
   return (
-    <AnimationOnScroll animateIn="animate__fadeInUp" duration=".5" offset="0" animateOnce>
-      <div className={styles.previewContainer}>
+    <AnimationOnScroll animateIn="animate__fadeInUp" duration=".5" offset="0" animateOnce className={styles.previewContainer}>
+      <Link href={`/portfolio/${experience.id}`}>
+        <a className={`${styles.linkStyle} transition-transform hover:scale-[1.02] active:scale-[.98]`}>
+          <Image
+            layout="fill"
+            alt={`${experience.data.title} thumbnail`}
+            className={styles.imageStyle}
+            objectFit={"cover"}
+            src={experience.data.thumbnail}
+            placeholder="blur"
+            blurDataURL={`/_next/image?url=${experience.data.thumbnail}&w=16&q=1`}/>/>
+        </a>
+      </Link>
+      <div className={styles.contentStyle}>
         <Link href={`/portfolio/${experience.id}`}>
-          <a className={`${styles.linkStyle} transition-transform hover:scale-[1.02] active:scale-[.98]`}>
-            <Image
-              layout="fill"
-              alt={`${experience.data.title} thumbnail`}
-              className={styles.imageStyle}
-              objectFit={"cover"}
-              src={experience.data.thumbnail}
-              placeholder="blur"
-              blurDataURL={`/_next/image?url=${experience.data.thumbnail}&w=16&q=1`}/>/>
-          </a>
+          <a className="text-2xl">{experience.data.title}</a>
         </Link>
-        <div className={styles.contentStyle}>
-          <Link href={`/portfolio/${experience.id}`}>
-            <a className="text-2xl">{experience.data.title}</a>
-          </Link>
-          <StartEndDateLabel startDate={experience.data.startDate} endDate={experience.data.endDate}/>
-          <p className={styles.description}>{experience.data.description}</p>
-        </div>
+        <StartEndDateLabel startDate={experience.data.startDate} endDate={experience.data.endDate}/>
+        <p className={styles.description}>{experience.data.description}</p>
       </div>
     </AnimationOnScroll>
   )
