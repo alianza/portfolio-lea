e5f13f81b7aae64c493e532900d0198e40780f0c
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2022-08-16 21:46:40 +0200

    Added next/future/image usage to footer.jsx

--- components/layout/footer/footer.jsx
+++ components/layout/footer/footer.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import styles from './footer.module.scss'
 import DarkModeButton from "../../darkModeButton/darkModeButton"
-import Image from 'next/image'
+import Image from 'next/future/image'
 
 export default function Footer({ accounts, onThemeButtonClick, darkTheme }) {
   return (
@@ -14,12 +14,12 @@ export default function Footer({ accounts, onThemeButtonClick, darkTheme }) {
         {accounts.map(account => (
           <a className="relative w-8 h-8 transition-transform hover:scale-110 active:scale-95" key={account.name} title={account.name} href={account.url} rel="noreferrer" target="_blank">
             <Image
-              layout="fill"
+              fill
+              sizes="100vw"
               alt={account.name}
               className="rounded"
               src={account.icon}
-              placeholder="blur"
-              blurDataURL={`/_next/image?url=${account.icon}&w=16&q=1`}/>
+            />
           </a>
         ))}
       </div>
