9a577f840be0d21693a95c6eba7e4fc3835e843e
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2024-02-05 18:30:24 +0100

    Fix typewrites spacing on mobile/tablet

--- components/typeWriter/typeWriter.jsx
+++ components/typeWriter/typeWriter.jsx
@@ -1,39 +1,41 @@
-import Typewriter from "typewriter-effect"
-import styles from "./typeWriter.module.scss"
-import { useEffect, useRef, useState } from "react"
+import Typewriter from 'typewriter-effect';
+import styles from './typeWriter.module.scss';
+import { useEffect, useRef, useState } from 'react';
 
 const TypeWriter = ({ quotes }) => {
-  const typewriterRef = useRef(null)
-  const [height, setHeight] = useState(88)
+  const typewriterRef = useRef(null);
+  const windowWidth = typeof window !== 'undefined' ? window.innerWidth : 0;
+  const [height, setHeight] = useState(windowWidth < 480 ? 80 : windowWidth < 600 ? 84 : 88);
 
   const getTypeWriterOptions = (typewriter, quotes) => {
-    const authorElem = document.getElementById("author")
-    const randomQuotes = quotes.sort(() => 0.5 - Math.random())
+    const authorElem = document.getElementById('author');
+    const randomQuotes = quotes.sort(() => 0.5 - Math.random());
 
     randomQuotes.forEach(({ author, text }) => {
-      typewriter.typeString(text)
+      typewriter
+        .typeString(text)
         .callFunction(() => {
-          authorElem.textContent = author
-          authorElem.style.opacity = '1'
-          updateHeight()
+          authorElem.textContent = author;
+          authorElem.style.opacity = '1';
+          updateHeight();
         })
         .pauseFor(text.length * 60)
         .deleteAll()
         .callFunction(() => {
-          authorElem.style.opacity = '0'
-          updateHeight()
+          authorElem.style.opacity = '0';
+          updateHeight();
         })
         .pauseFor(1000)
-        .start()
-    })
-  }
+        .start();
+    });
+  };
 
-  const updateHeight = () => setHeight(typewriterRef?.current?.clientHeight + 24)
+  const updateHeight = () => setHeight(typewriterRef?.current?.clientHeight + 24);
 
   useEffect(() => {
-    const interval = setInterval(() => updateHeight(), 500)
-    return () => clearInterval(interval)
-  }, [])
+    const interval = setInterval(() => updateHeight(), 500);
+    return () => clearInterval(interval);
+  }, []);
 
   return (
     <div className={styles.TypeWriter} style={{ height }}>
@@ -46,10 +48,12 @@ const TypeWriter = ({ quotes }) => {
             loop: true,
           }}
         />
-        <span id="author" className="opacity-0 transition-opacity duration-500 block h-6 font-text">Author</span>
+        <span id="author" className="block h-6 font-text opacity-0 transition-opacity duration-500">
+          Author
+        </span>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default TypeWriter
+export default TypeWriter;
