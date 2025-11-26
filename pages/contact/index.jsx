11784cc873d88b2cdc3e08f56163ca35dd69ec25
Jan-Willem van Bremen <janwillemvanbremen@live.nl>
2025-11-26 23:51:40 +0100

    Update Contact component to display email contact information and disable form

--- pages/contact/index.jsx
+++ pages/contact/index.jsx
@@ -1,32 +1,40 @@
-import utils from "../../styles/utils.module.scss"
-import Layout from "../../components/layout/layout/layout"
-import { getPage } from "../../lib/services/pageService"
-import MdContent from "../../components/mdContent/mdContent"
-import ContactFormNetlify from "../../components/contactForm/contactFormNetlify"
-import React from "react"
-import Hr from "../../components/layout/util/hr/hr";
+import utils from '../../styles/utils.module.scss';
+import Layout from '../../components/layout/layout/layout';
+import { getPage } from '../../lib/services/pageService';
+import MdContent from '../../components/mdContent/mdContent';
+import ContactFormNetlify from '../../components/contactForm/contactFormNetlify';
+import React from 'react';
+import Hr from '../../components/layout/util/hr/hr';
 
 export const getStaticProps = async () => {
-
-  const contactContent = await getPage("contact")
+  const contactContent = await getPage('contact');
 
   return {
     props: {
-      contactContent
-    }
-  }
-}
+      contactContent,
+    },
+  };
+};
 
 const Contact = ({ contactContent }) => {
   return (
     <div className={utils.page}>
-      <MdContent content={contactContent}/>
-      <Hr/>
-      <ContactFormNetlify/>
+      <MdContent content={contactContent} />
+      <Hr />
+      {/*<ContactFormNetlify/>*/} // todo: fix contact form https://docs.netlify.com/manage/forms/setup/
+      <div>
+        <h3 className="mb-4 text-xl font-semibold">Contact Form is currently not working unfortunately.</h3>
+        <p>
+          Please reach out via email at:{' '}
+          <a href="mailto:thecndle@gmail.com" className="text-blue-600 underline">
+            thecndle@gmail.com
+          </a>
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-Contact.withLayout = (page) => <Layout>{page}</Layout>
+Contact.withLayout = (page) => <Layout>{page}</Layout>;
 
 export default Contact;
